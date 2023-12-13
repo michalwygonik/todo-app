26df912f4907e8a97acedb4f8e02c09b64116854
michalwygonik <wygonikmichal@gmail.com>
2023-12-13 18:25:07 +0100

    prisma uninstall

--- prisma/migrations/20231016155319_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Task" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "title" TEXT NOT NULL,
-    "complete" BOOLEAN NOT NULL,
-    "createdAt" DATETIME NOT NULL,
-    "completedAt" DATETIME NOT NULL
-);
